0328d7ebddc9b07f876dd1e844a744a82034ceda
abhinav1141 <72382661+abhinav1141@users.noreply.github.com>
2021-06-04 18:27:22 +0530

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, FlatList,TouchableOpacity } from 'react-native';
+import { ListItem } from 'react-native-elements'
+import firebase from 'firebase';
+import db from '../config'
+import MyHeader from '../.expo/MyHeader';
+
+export default class BookDonateScreen extends Component{
+  constructor(){
+    super()
+    this.state = {
+      requestedBooksList : []
+    }
+  this.requestRef= null
+  }
+
+  getRequestedBooksList =()=>{
+    this.requestRef = db.collection("exchange_requests")
+    .onSnapshot((snapshot)=>{
+      var requestedBooksList = snapshot.docs.map(document => document.data());
+      this.setState({
+        requestedBooksList : requestedBooksList
+      });
+    })
+  }
+
+  componentDidMount(){
+    this.getRequestedBooksList()
+  }
+
+  componentWillUnmount(){
+    this.requestRef();
+  }
+
+  keyExtractor = (item, index) => index.toString()
+
+  renderItem = ( {item, i} ) =>{
+    return (
+      <ListItem
+        key={i}
+        title={item.item_name}
+        subtitle={item.description}
+        titleStyle={{ color:"black"}}
+        bottomDivider
+      />
+    )
+  }
+
+  render(){
+    return(
+      <View style={{flex:1}}>
+        <MyHeader title="BARTER SYSTEM"/>
+        <View style={{flex:1}}>
+          {
+            this.state.requestedBooksList.length === 0
+            ?(
+              <View style={styles.subContainer}>
+                <Text style={{ fontSize: 20}}>List Of All BARTER</Text>
+              </View>
+            )
+            :(
+              <FlatList
+                keyExtractor={this.keyExtractor}
+                data={this.state.requestedBooksList}
+                renderItem={this.renderItem}
+              />
+            )
+          }
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  subContainer:{
+    flex:1,
+    fontSize: 20,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  button:{
+    width:100,
+    height:30,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:"#ff5722",
+    shadowColor: "#000",
+    shadowOffset: {
+       width: 0,
+       height: 8
+     }
+  }
+})
